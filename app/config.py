eea331b839d2b9494a55af2532bd9503cb6f87ac
dnyt <lyricaldnyt@gmail.com>
2018-04-08 20:31:53 +0300

    Create child confiig classes

--- app/config.py
+++ app/config.py
@@ -5,3 +5,15 @@ class Config:
     for general configuration
     '''
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+class ProdConfig(Config):
+    '''
+    Child class for production
+    '''
+class Devconfig(Config):
+    '''
+    Child class for development
+    '''
+
+    DEBUG= True
+
+        
