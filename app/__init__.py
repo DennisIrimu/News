c6472b4fe7758ffd2a1f058f09421a03235a1696
dnyt <lyricaldnyt@gmail.com>
2018-04-08 20:55:21 +0300

    Register Blueprints

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    '''
+    Function that takes config Key as argument
+    '''
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+
+    from.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .requests import configure_request
+    
