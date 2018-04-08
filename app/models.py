8d646f3d5c46e0c1d9e9b71a9af278a4dc195de0
dnyt <lyricaldnyt@gmail.com>
2018-04-08 22:16:13 +0300

    Create class source

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Source:
+    '''
+    Class that defines Source Objects
+    '''
+    def __init__(self,id,name,description):
+        '''
+        define properties of the Source
+        '''
+        self.id = id
+        self.name = name
+        self.description = description
+
+        
