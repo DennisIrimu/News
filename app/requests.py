e3c5646f51c27be8d84011a370e7eb1ff23bf857
dnyt <lyricaldnyt@gmail.com>
2018-04-08 21:39:58 +0300

    Add function to work with JSON data

--- app/requests.py
+++ app/requests.py
@@ -13,4 +13,16 @@ def get_sources():
     '''
     get_sources_url = 'https://newsapi.org/v1/sources'
 
-    with urllib.request.urlopen(get_sources_data)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        geT_sources_response = json.loads(geT_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = geT_sources_response['sources']
+            sources_results = process_sources(sources_results_list)
+
+    return sources_results
+
+def process_sources()
