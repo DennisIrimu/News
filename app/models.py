56d5cc784ec46fcbfc653da7e25468d8e3a8bdbc
dnyt <lyricaldnyt@gmail.com>
2018-04-08 22:24:10 +0300

    define class article:

--- app/models.py
+++ app/models.py
@@ -10,4 +10,19 @@ class Source:
         self.name = name
         self.description = description
 
-class A
+class Article:
+    '''
+    Class that defines Article Objects
+    '''
+    def __init__(self,source,title,urlToImage,description,urlToArticle,publishedAt):
+        '''
+        define properties of an article
+        '''
+        self.source = source
+        self.title = title
+        self.urlToImage = urlToImage
+        self.description = description
+        self.urlToArticle = urlToArticle
+        self.publishedAt = publishedAt
+
+    
